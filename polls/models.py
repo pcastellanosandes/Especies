46a8c1e8e1a6ce15b3f68cd47f67ab8a995e5e42
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2017-02-07 20:36:24 -0500

    merge edit user view with new user's attribute. Following https://simpleisbetterthancomplex.com/tutorial/2016/11/23/how-to-add-user-profile-to-django-admin.html

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,24 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     imageFile = models.ImageField(upload_to='images', null=True)
     interest = models.TextField(max_length=1000, null=True)
+
+    def __str__(self):  # __unicode__ for Python 2
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
 
 
 # Create your models here.
