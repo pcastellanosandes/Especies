5e95b8af878d882c17c98ce1d6e041ab2ed834cf
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2017-02-07 20:10:15 -0500

    user administration. The model user was extended using https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone working with option 2

--- polls/admin.py
+++ polls/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from .models import Specie
 from .models import Category
 from .models import Comment
+from .models import Profile
+
 
 # Register your models here.
 admin.site.register(Specie)
 admin.site.register(Category)
 admin.site.register(Comment)
+admin.site.register(Profile)
+
+
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,12 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    imageFile = models.ImageField(upload_to='images', null=True)
+    interest = models.TextField(max_length=1000, null=True)
 
 
 # Create your models here.
