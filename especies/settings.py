e21bf1b15f1c3536cf85ba7b3d3d4e7ddaf5cd90
Armando Perea Mora <a.perea10@uniandes.edu.co>
2017-02-09 18:35:00 -0500

    Comentado "import dj_database_url" porque no dejaba compilar ¿Que librería se necesita intalar? Por favor hacer el comentario en el código Agregada aplicacion "bootstrap3"

--- especies/settings.py
+++ especies/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import dj_database_url
+#import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls'
+    'polls',
+    'bootstrap3',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -79,7 +80,7 @@ WSGI_APPLICATION = 'especies.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'NaturalReserve',
+        'NAME': 'especies',
         'USER': 'sid',
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
